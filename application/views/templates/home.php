ed0d17cf44840dafaf5b29794b86c2f8e98aec4f
Caio Castro <caiocacastro@gmail.com>
2015-11-20 09:39:29 -0200

    removing wrong merged files

--- application/views/templates/home.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('includes/header'); ?>
-<?php $this->load->view('includes/topbar'); ?>
-<?php $this->load->view('pages/index'); ?>
-<?php $this->load->view('includes/scripts'); ?>
-<?php $this->load->view('includes/footer'); ?>
--- application/views/templates/login.php
+++
@@ -1,4 +0,0 @@
-<?php $this->load->view('includes/header'); ?>
-<?php $this->load->view('pages/login'); ?>
-<?php $this->load->view('includes/scripts_login'); ?>
-<?php $this->load->view('includes/footer'); ?>
